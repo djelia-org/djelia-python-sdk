51d3a147a1b34bda237062ce7fb1e7078a5d56fe
sudoping01 <sudoping01@gmail.com>
2025-03-10 21:58:19 +0000

    init

---
+++ djelia/__init__.py
@@ -0,0 +1,19 @@
+from .client import Djelia
+from .constants import (
+    SUPPORTED_LANGUAGES,
+    VALID_SPEAKER_IDS
+)
+
+from .exceptions import (
+    DjeliaError,
+    AuthenticationError,
+    ValidationError,
+    APIError,
+    LanguageError,
+    SpeakerError,
+    AudioFormatError
+)
+
+__version__ = "0.1.0"
+
+__all__ = ['Djelia']
