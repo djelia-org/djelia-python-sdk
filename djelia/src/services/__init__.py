cf215708251af2857a3191053fff5d2cc30bd0b3
sudoping01 <sudoping01@gmail.com>
2025-06-01 03:49:54 +0000

    expose services allow issue import

---
+++ djelia/src/services/__init__.py
@@ -0,0 +1,12 @@
+from .transcription import Transcription, AsyncTranscription
+from .translation import Translation, AsyncTranslation
+from .tts import TTS, AsyncTTS
+
+__all__ = [
+    "Transcription",
+    "AsyncTranscription",
+    "Translation",
+    "AsyncTranslation",
+    "TTS",
+    "AsyncTTS"
+]
