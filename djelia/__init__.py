e1bb1ae0f599b23055a16e3f1ac04834915a3d62
sudoping01 <sudoping01@gmail.com>
2025-06-01 03:53:52 +0000

    expose only clients

--- djelia/__init__.py
+++ djelia/__init__.py
@@ -1,34 +1,3 @@
-from .client import Djelia, AsyncDjelia
-from .constants import djelia_config
+from djelia.src.client import Djelia, DjeliaAsync
 
-
-SUPPORTED_LANGUAGES = djelia_config.SUPPORTED_LANGUAGES
-VALID_SPEAKER_IDS = djelia_config.VALID_SPEAKER_IDS
-
-from .exceptions import (
-    DjeliaError,
-    AuthenticationError,
-    ValidationError,
-    APIError,
-    LanguageError,
-    SpeakerError,
-    AudioFormatError
-)
-
-__version__ = "0.2.0"
-
-__all__ = [
-    'Djelia',
-    'AsyncDjelia',
-    'SUPPORTED_LANGUAGES',
-    'VALID_SPEAKER_IDS'
-]
-
-    # let's close those component
-    # 'DjeliaError',
-    # 'AuthenticationError',
-    # 'ValidationError',
-    # 'APIError',
-    # 'LanguageError',
-    # 'SpeakerError',
-    # 'AudioFormatError'
+__all__ = ["Djelia", "DjeliaAsync"]
