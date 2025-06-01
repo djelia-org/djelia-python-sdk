a8b7ab49f1d7751ba2dbe96e14664ad76dde4448
sudoping01 <sudoping01@gmail.com>
2025-06-01 03:49:25 +0000

    translation service wit sync and async operator whiout stremaing

---
+++ djelia/src/services/translation.py
@@ -0,0 +1,45 @@
+
+from typing import List, Optional
+from djelia.models import (DjeliaRequest, TranslationRequest, TranslationResponse,
+                          SupportedLanguageSchema, Versions)
+
+class Translation:
+    def __init__(self, client):
+        self.client = client
+
+    def get_supported_languages(self) -> List[SupportedLanguageSchema]:
+        response = self.client._make_request(
+            method=DjeliaRequest.get_supported_languages.method,
+            endpoint=DjeliaRequest.get_supported_languages.endpoint.format(Versions.v1.value)
+        )
+        return [SupportedLanguageSchema(**lang) for lang in response.json()]
+
+    def translate(self, request: TranslationRequest, version: Optional[Versions] = Versions.v1.value) -> TranslationResponse:
+        data = request.dict()
+        response = self.client._make_request(
+            method=DjeliaRequest.translate.method,
+            endpoint=DjeliaRequest.translate.endpoint.format(version.value),
+            json=data
+        )
+        return TranslationResponse(**response.json())
+
+
+class AsyncTranslation:
+    def __init__(self, client):
+        self.client = client
+
+    async def get_supported_languages(self) -> List[SupportedLanguageSchema]:
+        data = await self.client._make_request(
+            method=DjeliaRequest.get_supported_languages.method,
+            endpoint=DjeliaRequest.get_supported_languages.endpoint.format(Versions.v1.value)
+        )
+        return [SupportedLanguageSchema(**lang) for lang in data]
+
+    async def translate(self, request: TranslationRequest, version: Optional[Versions] = Versions.v1) -> TranslationResponse:
+        request_data = request.dict()
+        data = await self.client._make_request(
+            method=DjeliaRequest.translate.method,
+            endpoint=DjeliaRequest.translate.endpoint.format(version.value),
+            json=request_data
+        )
+        return TranslationResponse(**data)
