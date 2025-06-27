86f12cff0db0ab2d1a1fda7b3a8581bbc1a2ef38
sudoping01 <sudoping01@gmail.com>
2025-06-27 18:43:41 +0000

    update version to a medium based on the changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="djelia",
-    version="1.0.1",
+    version="1.1.1",
     author="Djelia",
     author_email="support@djelia.cloud",
     description="Djelia Python SDK - Advanced AI for African Languages",
